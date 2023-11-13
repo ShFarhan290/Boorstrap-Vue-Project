ffbcd0d7360f261abbee9d4d15014f821744a677
ShFarhan290 <mdhachan1950@gmail.com>
2023-11-13 21:54:16 +0600

    BootstrapWithVue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Header from './components/Header.vue'
+import Carousel from './components/Carousel.vue'
+import Hero from './components/Hero.vue'
+import BlogPost from './components/BlogPost.vue'
+import Pricing from './components/Pricing.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <Header></Header>
+  <Carousel></Carousel>
+  <Hero></Hero>
+  <BlogPost></BlogPost>
+  <Pricing></Pricing>
+  <Footer></Footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,61 @@
+<script>
+</script>
+
+<template>
+
+    <div class="BlogPost">
+
+        <div class="row mb-2">
+            <div class="col-md-6">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div class="col p-4 d-flex flex-column position-static">
+                        <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong>
+                        <h3 class="mb-0">BMW 1000rr</h3>
+                        <div class="mb-1 text-body-secondary">Nov 12</div>
+                        <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                            Continue reading
+                            <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+                        </a>
+                    </div>
+                    <div class="col-auto d-none d-lg-block">
+                        <img src="https://images.pexels.com/photos/8634550/pexels-photo-8634550.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" style="width: 200px; height: 260px; background-size: contain;">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div class="col p-4 d-flex flex-column position-static">
+                        <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
+                        <h3 class="mb-0">Kawasaki Ninja</h3>
+                        <div class="mb-1 text-body-secondary">Nov 11</div>
+                        <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                            Continue reading
+                            <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+                        </a>
+                    </div>
+                    <div class="col-auto d-none d-lg-block">
+                        <img src="https://images.pexels.com/photos/10392245/pexels-photo-10392245.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" style="width: 200px; height: 260px; background-size: contain;">
+                    </div>
+                </div>
+            </div>
+     </div>
+
+    </div>
+
+</template>
+
+<style scoped>
+.row {
+    width: 100%;
+    margin-left: 0px;
+    margin-top: 20px;
+}
+.BlogPost{
+    width: 100%;
+    height: auto;
+    padding: 30px;
+    background-color: azure;
+}
+</style>
